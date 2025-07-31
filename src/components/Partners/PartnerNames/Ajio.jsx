ae4a16745bb994d7a269a718c8d76b2213b4ae30
tejaswimvm <tejaswi.mvmbs@gmail.com>
2025-07-31 18:19:49 +0530

    Updates

--- src/components/Partners/PartnerNames/Ajio.jsx
+++ src/components/Partners/PartnerNames/Ajio.jsx
@@ -9,7 +9,7 @@ function Ajio() {
       keydesc: `  Reliance's well known online fashion store, AJIO, sells stylish yet reasonably priced apparel for men, women, and children. It offers a large selection of both Indian and foreign brands, fashionable collections, simple return policies, and regular deals. Customers who are interested in fashion frequent it because of its well chosen edits and premium brands.`,
       benifirst: "/images/ajiobenifit.webp",
       benisec: "/images/ajiobenifit.webp",
-      site: `https://www.ajio.com/s/new30-5567-56041?utm_term=&clickid=68385dd394456e000191a675&pid=87&offer_id=2&attribution_window=1D&return_cancellation_window=45D`,
+      site: `https://tracking.ajio.business/click?pid=87&offer_id=2&sub1=pass_your_subid%20&redirect=https://www.ajio.com/s/50to90percentoff-140961`,
       pros: [
         "Provides leading fashion brands from India and abroad.",
         "Regular sales, discounts, and pocketbook deals",
